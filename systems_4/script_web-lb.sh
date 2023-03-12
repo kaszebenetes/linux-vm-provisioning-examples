c38d43add9170f5e297438bbd524798f843b5b16
kobeep <kuba.pospieszny@gmail.com>
2023-03-12 18:34:31 +0100

    Still improvements of scripts

--- systems_4/script_web-lb.sh
+++ systems_4/script_web-lb.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+# set -x
 set -euo pipefail
-list_installed="$(sudo yum list installed | grep 'haproxy')"
-if [ "${list_installed}" = "" ]
+
+# set -e 
+
+### vars
+
+HAPROXY_CONFIG_PATH=/etc/haproxy/haproxy.cfg
+TEMP_HAPROXY_CONFIG_PATH=/tmp/haproxy.cfg
+###
+
+echo "==> Info: Checking if haproxy is installed"
+if yum list installed | grep -q 'haproxy'
 then
+    echo "Haproxy package is already installed, proceeding..."
+else
     echo "Installing haproxy service:"
     sudo yum -y install haproxy
-else
-    echo "Haproxy service is already installed."
-
 fi
-rm -f /etc/haproxy/haproxy.cfg
-touch /etc/haproxy/haproxy.cfg
+
 echo "
 global
    log /dev/log local0
@@ -40,16 +48,54 @@ backend http_back
    balance roundrobin
    server web-1 10.0.0.21:80 check
    server web-2 10.0.0.22:80 check
-" >> /etc/haproxy/haproxy.cfg
-STATUS="$(systemctl is-active haproxy.service)"
-if [ "${STATUS}" = "inactive" ] 
+" > /tmp/haproxy.cfg
+
+
+if [ -f $HAPROXY_CONFIG_PATH ]
+then
+    echo "Haproxy config exists, proceeding..."
+    
+    TEMP_HAPROXY_CONFIG_CHECKSUM=$(sha256sum $TEMP_HAPROXY_CONFIG_PATH | awk '{print $1}')
+    echo "TEMP_HAPROXY_CONFIG_CHECKSUM=$TEMP_HAPROXY_CONFIG_CHECKSUM"
+
+    HAPROXY_CONFIG_CHECKSUM=$(sha256sum $HAPROXY_CONFIG_PATH | awk '{print $1}')
+    echo "HAPROXY_CONFIG_CHECKSUM=$HAPROXY_CONFIG_CHECKSUM"
+
+    if [ "$TEMP_HAPROXY_CONFIG_CHECKSUM" != "$HAPROXY_CONFIG_CHECKSUM" ]
+    then
+        echo "Checksum differs, overriding config file!"
+        mv $TEMP_HAPROXY_CONFIG_PATH $HAPROXY_CONFIG_PATH
+
+        HAPROXY_CONFIG_CHANGED_FLAG=true
+    else
+        echo "Checksum is the same, haproxy config file not modified"
+
+        HAPROXY_CONFIG_CHANGED_FLAG=false
+    fi
+else
+    echo "Haproxy config file doesn't exist, creating..."
+    mv $TEMP_HAPROXY_CONFIG_PATH $HAPROXY_CONFIG_PATH
+fi
+
+echo
+echo "Checking if haproxy service is running"
+# systemctl is-active return exit code 3 if service is not active
+STATUS="$(systemctl is-active haproxy.service; exit 0)"
+if [[ $STATUS != "active" ]]
 then 
     echo "Starting haproxy sevice:"
     systemctl start haproxy.service
     systemctl enable haproxy.service
-
-else 
-    echo "Haproxy was active. Restarting service:"
-    systemctl restart haproxy.service
-    systemctl status haproxy.service
 fi
+
+if [ $HAPROXY_CONFIG_CHANGED_FLAG == true ]
+then
+    echo "Restarting haproxy.service as config has changed!"
+    systemctl restart haproxy.service
+else
+    echo "Restart not needed, proceeding..."
+fi
+
+echo
+echo "Printing haproxy.service status"
+systemctl status haproxy.service
--- systems_4/script_web.sh
+++ systems_4/script_web.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
 
+set -x
 set -euo pipefail
-REPOLIST=$(yum repolist -q | grep epel-release)
-if ${REPOLIST}; then
+
+###Vars
+
+NGINX_CONFIG_PATH=/etc/nginx/nginx.conf
+
+NGINX_CONFIG_PATH_CHECK=$(sha256sum $NGINX_CONFIG_PATH)
+
+NAME_OF_MACHINE=$(cat /etc/hostname)
+
+STATUS=$(systemctl is-active nginx.service; exit 0)
+
+LIST=${sudo yum list installed | grep nginx}
+###
+
+if [ yum repolist | grep -q 'epel-release';exit 0 ] 
+then
+    echo "Repository epel is already installed."
+else
     echo "Installing repository epel:"
     sudo yum -y install epel-release
-else
-    echo "Repository epel is already installed."
 fi
+
+
 yum makecache
-LIST=${sudo yum list installed | grep nginx}
-if [ "${LIST}" = Null ]; then
+
+if [ "${LIST}" = Null ] 
+then
     echo "Installing nginx service:"
     sudo yum -y install nginx
 else
     echo "Nginx service is already installed."
 
 fi
-NGINX_CONFIG_STATUS=$(sha256sum /etc/nginx/nginx.conf)
-OLD_NGINX_CONFIG_STATUS="38e3821e3cee16c619b0dc5ac0a76d50c0630b09f592c5251aa747abf7bfb537  nginx.conf"
-rm -rf /usr/share/nginx/html/index.html
-echo "<body><h1>serwer1</h1></body>" >> /usr/share/nginx/html/index.html
-STATUS=$(systemctl is-active nginx.service)
-if [ "${STATUS}" = "inactive" ]; then 
-    echo"Starting nginx sevice:"
-    systemctl start nginx.service
-    systemctl enable nginx.service
 
-elif [ "${NGINX_CONFIG_STATUS}" != "${OLD_NGINX_CONFIG_STATUS}" ]; then
-    echo "Nginx configuration file has been changed. Restarting service:"
-    systemctl restart nginx.service
-    systemctl status nginx.service
- else 
-    echo "Nginx is active and everything is up to date."
-fi
+mv $NGINX_CONFIG_PATH $TMP_NGINX_CONFIG_PATH
+
+TMP_NGINX_CONFIG_PATH=/tmp/nginx.conf
+
+TMP_NGINX_CONFIG_PATH_CHECK=$(sha256sum /tmp/nginx.conf)
+
+# if [ "${STATUS}" != "active" ] 
+# then 
+#     echo"Nginx was inactive. Starting ..."
+#     systemctl start nginx.service
+#     systemctl enable nginx.service
+# elif [ "${STATUS}" = "active" && $NGINX_CONFIG_PATH_CHECK =! $TMP_NGINX_CONFIG_PATH_CHECK ]
+#     echo "Nginx config file was changed. Needed restart of this service. Proceeding.."
+#     systemctl restart nginx.service
+# else
+    
+# fi
+
+systemctl status nginx.service
+
+rm -rf /usr/share/nginx/html/index.html
+echo "<body><h1>$NAME_OF_MACHINE</h1></body>" >> /usr/share/nginx/html/index.html
+
