a28c86bbdf49e2c237fe3f74bd002752791baf2a
Jakub Pospieszny <102924040+Kobeep@users.noreply.github.com>
2023-03-12 18:45:16 +0100

    Improvement of scripts (#1)
    * Improvement of scripts
    
    * add hostname
    
    * Improving scripts and automating them
    
    * Script.sh removed.
    
    * Still improvements of scripts
    
    * change directory name, add some echoes
    
    * Remove nginx changes
    
    ---------
    
    Co-authored-by: mikeeq <miotk.mikolaj@gmail.com>

---
+++ vagrant_haproxy_tcp_nginx/script_web-lb.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Debug
+# set -x
+
+set -euo pipefail
+
+### vars
+
+HAPROXY_CONFIG_PATH=/etc/haproxy/haproxy.cfg
+TEMP_HAPROXY_CONFIG_PATH=/tmp/haproxy.cfg
+###
+
+echo "==> Info: Checking if haproxy is installed"
+if yum list installed | grep -q 'haproxy'
+then
+    echo "Haproxy package is already installed, proceeding..."
+else
+    echo "Installing haproxy service:"
+    yum -y install haproxy
+fi
+
+echo "
+global
+   log /dev/log local0
+   log /dev/log local1 notice
+   chroot /var/lib/haproxy
+   stats timeout 30s
+   user haproxy
+   group haproxy
+   daemon
+
+defaults
+   log global
+   mode tcp
+   option httplog
+   option dontlognull
+   timeout connect 5000
+   timeout client 50000
+   timeout server 50000
+
+frontend http_front
+   bind *:80
+   stats uri /haproxy?stats
+   default_backend http_back
+
+backend http_back
+   balance roundrobin
+   server web-1 10.0.0.21:80 check
+   server web-2 10.0.0.22:80 check
+" > /tmp/haproxy.cfg
+
+echo
+echo "==> Info: Checking haproxy config"
+if [ -f $HAPROXY_CONFIG_PATH ]
+then
+    echo "Haproxy config exists, proceeding..."
+
+    TEMP_HAPROXY_CONFIG_CHECKSUM=$(sha256sum $TEMP_HAPROXY_CONFIG_PATH | awk '{print $1}')
+    echo "TEMP_HAPROXY_CONFIG_CHECKSUM=$TEMP_HAPROXY_CONFIG_CHECKSUM"
+
+    HAPROXY_CONFIG_CHECKSUM=$(sha256sum $HAPROXY_CONFIG_PATH | awk '{print $1}')
+    echo "HAPROXY_CONFIG_CHECKSUM=$HAPROXY_CONFIG_CHECKSUM"
+
+    if [ "$TEMP_HAPROXY_CONFIG_CHECKSUM" != "$HAPROXY_CONFIG_CHECKSUM" ]
+    then
+        echo "Checksum differs, overriding config file!"
+        mv $TEMP_HAPROXY_CONFIG_PATH $HAPROXY_CONFIG_PATH
+
+        HAPROXY_CONFIG_CHANGED_FLAG=true
+    else
+        echo "Checksum is the same, haproxy config file not modified"
+
+        HAPROXY_CONFIG_CHANGED_FLAG=false
+    fi
+else
+    echo "Haproxy config file doesn't exist, creating..."
+    mv $TEMP_HAPROXY_CONFIG_PATH $HAPROXY_CONFIG_PATH
+fi
+
+echo
+echo "==> Info: Checking if haproxy service is running"
+# systemctl is-active return exit code 3 if service is not active
+STATUS="$(systemctl is-active haproxy.service; exit 0)"
+if [[ $STATUS != "active" ]]
+then
+    echo "Starting haproxy sevice:"
+    systemctl start haproxy.service
+    systemctl enable haproxy.service
+fi
+
+if [ $HAPROXY_CONFIG_CHANGED_FLAG == true ]
+then
+    echo "Restarting haproxy.service as config has changed!"
+    systemctl restart haproxy.service
+else
+    echo "Restart not needed, proceeding..."
+fi
+
+echo
+echo "==> Info: Printing haproxy.service status"
+systemctl status haproxy.service
