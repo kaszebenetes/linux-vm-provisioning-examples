0598596686b3ddd6da9dcda49980aea4de3041ca
kobeep <kuba.pospieszny@gmail.com>
2023-03-13 13:26:06 +0100

    Already done it. :p

--- vagrant_haproxy_tcp_nginx/script_web.sh
+++ vagrant_haproxy_tcp_nginx/script_web.sh
@@ -1,61 +1,73 @@
 #!/bin/bash
 
-set -x
-set -euo pipefail
+# set -x
+# set -euo pipefail
 
 ###Vars
 
-NGINX_CONFIG_PATH=/etc/nginx/nginx.conf
-
-NGINX_CONFIG_PATH_CHECK=$(sha256sum $NGINX_CONFIG_PATH)
-
 NAME_OF_MACHINE=$(cat /etc/hostname)
 
-STATUS=$(systemctl is-active nginx.service; exit 0)
-
-LIST=${sudo yum list installed | grep nginx}
 ###
 
-if [ yum repolist | grep -q 'epel-release';exit 0 ] 
+if yum repolist | grep -q epel
 then
-    echo "Repository epel is already installed."
+    echo "==> Info: Repository epel is already installed."
 else
-    echo "Installing repository epel:"
+    echo "==> Info: Installing repository epel:"
     sudo yum -y install epel-release
 fi
 
 
 yum makecache
 
-if [ "${LIST}" = Null ] 
+if yum list installed | grep -q nginx
 then
-    echo "Installing nginx service:"
-    sudo yum -y install nginx
+    echo "==> Info: Nginx service is already installed."
 else
-    echo "Nginx service is already installed."
-
+    
+    echo "==> Info: Installing Nginx service:"
+    sudo yum -y install nginx
 fi
 
-mv $NGINX_CONFIG_PATH $TMP_NGINX_CONFIG_PATH
+STATUS=$(systemctl is-active nginx.service; exit 0)
 
-TMP_NGINX_CONFIG_PATH=/tmp/nginx.conf
+if [ "${STATUS}" = "active" ] 
+then
+    echo "==> Info: Nginx service is working..." 
+else
+    echo "==> Info: Nginx was inactive. Starting service..."
+    systemctl start nginx.service
+    systemctl enable nginx.service
+fi
 
-TMP_NGINX_CONFIG_PATH_CHECK=$(sha256sum /tmp/nginx.conf)
-
-# if [ "${STATUS}" != "active" ] 
-# then 
-#     echo"Nginx was inactive. Starting ..."
-#     systemctl start nginx.service
-#     systemctl enable nginx.service
-# elif [ "${STATUS}" = "active" && $NGINX_CONFIG_PATH_CHECK =! $TMP_NGINX_CONFIG_PATH_CHECK ]
-#     echo "Nginx config file was changed. Needed restart of this service. Proceeding.."
-#     systemctl restart nginx.service
-# else
-    
-# fi
-
+echo
+echo "==> Info: Nginx.service status:"
 systemctl status nginx.service
+echo "<=="
+echo
 
-rm -rf /usr/share/nginx/html/index.html
-echo "<body><h1>$NAME_OF_MACHINE</h1></body>" >> /usr/share/nginx/html/index.html
+###Interior index.html file:
+echo "<body><h1>$NAME_OF_MACHINE</h1></body>" > /tmp/index.html
+###
+
+###VarsNGINX
+
+TMP_INDEX_PATH=/tmp/index.html
+INDEX_PATH=/usr/share/nginx/html/index.html
+CHECK_INDEX=$(sha256sum /usr/share/nginx/html/index.html | awk '{print $1}')
+TMP_CHECK_INDEX=$(sha256sum /tmp/index.html | awk '{print $1}')
+###
+
+if [ -f $INDEX_PATH ] && [ "$CHECK_INDEX" = "$TMP_CHECK_INDEX" ]
+then
+    echo "==> Info: Index.html file exists. No differs in file."
+elif [ -f $INDEX_PATH ] && [ "$CHECK_INDEX" != "$TMP_CHECK_INDEX" ]
+then
+    echo "==> Info: File Index.html has changed. Reloading service..."
+    mv $TMP_INDEX_PATH $INDEX_PATH
+    systemctl reload nginx
+else
+    echo "==> Info: Index.html doesnt exists. Inserting file..."
+    mv $TMP_INDEX_PATH $INDEX_PATH
+fi
 
