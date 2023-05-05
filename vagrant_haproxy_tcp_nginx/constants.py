a9bd620702d1dfe1263c2f2f0ac7e87a2ed02b3d
kobeep <kuba.pospieszny@gmail.com>
2023-05-05 23:07:54 +0200

    Init version.Do not include this file

--- vagrant_haproxy_tcp_nginx/constants.py
+++ vagrant_haproxy_tcp_nginx/constants.py
@@ -1,28 +1,29 @@
 import subprocess
-
+import os
+import time
 
 # Repository which is required by loading-bar
-tqdm = subprocess.run("yum repolist | grep 'tqdm'")
+# tqdm = os.system("dnf repolist | grep 'tqdm'")
 #
 
 # SSH commands to vms..
-sshtobastion = subprocess.run("ssh -L -q vagrant@10.0.0.111")
+sshtobastion = os.system("ssh -L vagrant@10.0.0.111")
 
-sshbastiontoweblb = subprocess.run("ssh -R -q 8080:10.0.0.11:22 vagrant@10.0.0.111")
+sshbastiontoweblb = os.system("ssh -R 22:10.0.0.11:8080 vagrant@10.0.0.111")
 
-sshbastiontoweb1 = subprocess.run("ssh -R -q 8080:10.0.0.21:22 vagrant@10.0.0.111")
+sshbastiontoweb1 = os.system("ssh -R 8080:10.0.0.21:22 vagrant@10.0.0.111")
 
-sshbastiontoweb2 = subprocess.run("ssh -R -q 8080:10.0.0.22:22 vagrant@10.0.0.111")
+sshbastiontoweb2 = os.system("ssh -R 8080:10.0.0.22:22 vagrant@10.0.0.111")
 #
 
 # Checking if iptables have been saved..
-iptablesbastion = subprocess.run("ssh -L vagrant@10.0.0.111 | /etc/sysconfig/iptables")
+iptablesbastion = os.system("ssh -L vagrant@10.0.0.111 | /etc/sysconfig/iptables")
 
-iptablesweblb = subprocess.run("ssh -R 22:10.0.0.11:22 vagrant@10.0.0.111 | -f /etc/sysconfig/iptables")
+iptablesweblb = os.system("ssh -R 22:10.0.0.11:22 vagrant@10.0.0.111 | -f /etc/sysconfig/iptables")
 
-iptablesweb1 = subprocess.run("ssh -R 22:10.0.0.21:22 vagrant@10.0.0.111 | -f /etc/sysconfig/iptables")
+iptablesweb1 = os.system("ssh -R 22:10.0.0.21:22 vagrant@10.0.0.111 | -f /etc/sysconfig/iptables")
 
-iptablesweb2 = subprocess.run("ssh -R 22:10.0.0.22:22 vagrant@10.0.0.111 | -f /etc/sysconfig/iptables")
+iptablesweb2 = os.system("ssh -R 22:10.0.0.22:22 vagrant@10.0.0.111 | -f /etc/sysconfig/iptables")
 
 
-iptables=subprocess.run("ssh -L vagrant@10.0.0.111 | iptables -L")
+iptables=os.system("ssh -L vagrant@10.0.0.111 | iptables -L")
