5544bc54897469ebfdc88550f676d4a749c3bd8d
Kobeep <kuba.pospieszny@gmail.com>
2023-07-19 13:29:24 +0200

    fix

--- vagrant_haproxy_tcp_nginx_shell/tests/test.sh
+++ vagrant_haproxy_tcp_nginx_shell/tests/test.sh
@@ -8,73 +8,66 @@
 
 # Testing ssh connection to bastion.
 
-ssh vagrant@bastion exit 0
-if [ $(echo $?) = "0" ]
+
+if [ $(ssh -q -o BatchMode=yes -o ConnectTimeout=5 bastion "exit") -eq 0 ]
 then
-    echo "==> INFO: SSH connection to bastion is avaliable for you."
+    echo "==> INFO: ✔️ SSH connection to bastion is avaliable for you."
 else
-    echo "==> INFO: Something went wrong.. No access to bastion."
+    echo "==> INFO: ❌ Something went wrong.. No access to bastion."
 fi
 
 # Testing ssh connection through bastion to another vms.
 
-# Vars:
-var points = 0
-#
-ssh vagrant@web-1 exit 0
-if [ $(echo $?) = "0" ]
+
+
+if [ $(ssh -q -o BatchMode=yes -o ConnectTimeout=5 web-1 "exit") -eq 0 ]
 then
-    echo "==> INFO: SSH connection to web1 is avaliable for you by bastion."
-    points+=1
+    echo "==> INFO: ✔️ SSH connection to web-1 is avaliable for you by bastion."
 else
-    echo "==> INFO: Something went wrong.. No access to web-1."
+    echo "==> INFO: ❌ Something went wrong.. No access to web-1."
 fi
 
-ssh vagrant@web-2 exit 0
-if [ $(echo $?) = "0" ]
+if [ $(ssh -q -o BatchMode=yes -o ConnectTimeout=5 web-2 "exit") -eq 0 ]
 then
-    echo "==> INFO: SSH connection to web2 is avaliable for you by bastion."
-    points+=1
+    echo "==> INFO: ✔️ SSH connection to web-2 is avaliable for you by bastion."
 else
-    echo "==> INFO: Something went wrong.. No access to web-2."
+    echo "==> INFO: ❌ Something went wrong.. No access to web-2."
 fi
 
-ssh vagrant@web-lb exit 0
-if [ $(echo $?) = "0" ]
+if [ $(ssh -q -o BatchMode=yes -o ConnectTimeout=5 web-lb "exit") -eq 0 ]
 then
-    echo "==> INFO: SSH connection to weblb is avaliable for you by bastion."
-    points+=1
+    echo "==> INFO: ✔️ SSH connection to web-lb is avaliable for you by bastion."
 else
-    echo "==> INFO: Something went wrong.. No access to web-lb."
+    echo "==> INFO: ❌ Something went wrong.. No access to web-lb."
 fi
 
-if [ "$(ssh vagrant@Bastion " ls /etc/sysconfig/ | grep 'iptables'")" ]
+# Checking if iptables have been stored..
+if [ "$(ssh vagrant@Bastion " cat /etc/sysconfig/iptables ")" = "" ]
 then
-    echo "==> INFO: Bastion iptables have been stored."
-    points+=1
+    echo "==> INFO: ❌ Bastion iptables haven't been stored."
 else
-    echo "==> INFO: Bastion iptables haven't been stored."
-fi
-echo "In ssh test part u got:$points "
-
-
-if [ "$(ssh -A -J vagrant@web-lb vagrant@Bastion "ls /etc/sysconfig/ | grep 'iptables'")" ]
-then
-    echo "==> INFO: web-lb's iptables have been stored."
-else
-    echo "==> INFO: web-lb's iptables haven't been stored."
+    echo "==> INFO: ✔️ Bastion iptables have been stored."
 fi
 
-if [ "$(ssh -A -J vagrant@Web-lb vagrant@Bastion "ls /etc/sysconfig/ | grep 'iptables'")" ]
+
+
+if [ "$(ssh vagrant@web-lb " cat /etc/sysconfig/iptables ")" = "" ]
 then
-    echo "==> INFO: Web-1's iptables have been stored."
+    echo "==> INFO: ❌ Web-lb's iptables haven't been stored."
 else
-    echo "==> INFO: Web-1's iptables haven't been stored."
+    echo "==> INFO: ✔️ Web-lb's iptables have been stored."
 fi
 
-if [ "$(ssh -A -J vagrant@Web-lb vagrant@Bastion "ls /etc/sysconfig/ | grep 'iptables'")" ]
+if [ "$(ssh vagrant@web-1 " cat /etc/sysconfig/iptables ")" = "" ]
 then
-    echo "==> INFO: Web-2's iptables have been stored."
+    echo "==> INFO: ❌ Web-1's iptables have been stored."
 else
-    echo "==> INFO: Web-2's iptables haven't been stored."
+    echo "==> INFO: ✔️ Web-1's iptables haven't been stored."
+fi
+
+if [ "$(ssh vagrant@web-2 " cat /etc/sysconfig/iptables ")" = "" ]
+then
+    echo "==> INFO: ❌ Web-2's iptables have been stored."
+else
+    echo "==> INFO: ✔️ Web-2's iptables haven't been stored."
 fi
